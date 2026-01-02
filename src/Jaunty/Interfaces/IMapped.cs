df183e62940126d3f2687e761fc231b3ceaa2a20
Syed Beparey <syed@beparey.com>
2026-01-02 20:00:05 +0800

    Added EntityReader and solved the problem of IMapped / user generated mapper for entities.

---
+++ src/Jaunty/Interfaces/IMapped.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+
+namespace Jaunty.Interfaces
+{
+    public interface IMapped<T> where T : IMapped<T>, new()
+    {
+#if NET7_0_OR_GREATER
+    static abstract T ReadEntity(IDataReader reader, ReadOnlySpan<int> ordinals);
+    static abstract ReadOnlySpan<string> ColumnNames { get; }
+#else
+        T ReadEntity(IDataReader reader, int[] ordinals);
+        string[] GetColumnNames();
+#endif
+    }
+}
--- tests/Jaunty.Tests/Entities/Product.cs
+++ tests/Jaunty.Tests/Entities/Product.cs
@@ -1,15 +1,52 @@
+using System.Data;
+
+using Jaunty.Attributes;
+using Jaunty.Interfaces;
+
 namespace Jaunty.Tests.Entities;
 
-public class Product
+public class Product : IEntity<int>, IMapped<Product>
 {
-    public long ProductId { get; set; }
-    public string ProductName { get; set; } = string.Empty;
-    public long? SupplierId { get; set; }
-    public long? CategoryId { get; set; }
+    public static ReadOnlySpan<string> ColumnNames => new("ProductID", "ProductName", "SupplierID", "CategoryID", "QuantityPerUnit", "UnitPrice", "UnitsInStock", "UnitsOnOrder", "ReorderLevel", "Discontinued");
+
+    [Ignore]
+    public int Id
+    {
+        get => ProductId;
+        set => ProductId = value;
+    }
+
+    public int ProductId { get; set; }
+
+    public string ProductName { get; set; } = null!;
+
+    public int? SupplierId { get; set; }
+
+    public short? CategoryId { get; set; }
+
     public string? QuantityPerUnit { get; set; }
-    public double? UnitPrice { get; set; }
+
+    public decimal? UnitPrice { get; set; }
+
     public short? UnitsInStock { get; set; }
+
     public short? UnitsOnOrder { get; set; }
+
     public short? ReorderLevel { get; set; }
+
     public bool Discontinued { get; set; }
+
+    public static Product ReadEntity(IDataReader reader, ReadOnlySpan<int> ordinals) => new()
+    {
+        ProductId = reader.GetInt32(ordinals[0]),
+        ProductName = reader.GetString(ordinals[1]),
+        SupplierId = reader.IsDBNull(ordinals[2]) ? null : reader.GetInt32(ordinals[2]),
+        CategoryId = reader.IsDBNull(ordinals[3]) ? null : reader.GetInt16(ordinals[3]),
+        QuantityPerUnit = reader.IsDBNull(ordinals[4]) ? null : reader.GetString(ordinals[4]),
+        UnitPrice = reader.IsDBNull(ordinals[5]) ? null : reader.GetDecimal(ordinals[5]),
+        UnitsInStock = reader.IsDBNull(ordinals[6]) ? null : reader.GetInt16(ordinals[6]),
+        UnitsOnOrder = reader.IsDBNull(ordinals[7]) ? null : reader.GetInt16(ordinals[7]),
+        ReorderLevel = reader.IsDBNull(ordinals[8]) ? null : reader.GetInt16(ordinals[8]),
+        Discontinued = reader.GetBoolean(ordinals[9])
+    };
 }
