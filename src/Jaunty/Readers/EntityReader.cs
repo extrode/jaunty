68ab8c9e42f2267fd1acfb703dd6b1492332080c
Syed Beparey <sy_github@maskx.com>
2026-01-03 14:46:09 +0800

    refactor: remove the superseded entity mapper and metadata cache

--- src/Jaunty/Readers/EntityReader.cs
+++ src/Jaunty/Readers/EntityReader.cs
@@ -5,11 +5,10 @@ using System.Runtime.CompilerServices;
 using Jaunty.Interfaces;
 namespace Jaunty.Readers;
 
-public static class EntityReader
+internal static class EntityReader
 {
     // Sync version - always available
-    public static IEnumerable<T> ReadEntities<T>(IDataReader reader)
-        where T : IMapped<T>, new()
+    internal static IEnumerable<T> ReadEntities<T>(IDataReader reader) where T : IMapped<T>, new()
     {
 #if NET8_0_OR_GREATER
         var columnNames = T.ColumnNames;
@@ -38,8 +37,7 @@ public static class EntityReader
     }
 
 #if NET8_0_OR_GREATER || ASYNC_ENUMERABLE_SUPPORT
-    public static async IAsyncEnumerable<T> ReadEntitiesAsync<T>(DbDataReader reader, [EnumeratorCancellation] CancellationToken cancellationToken = default)
-        where T : IMapped<T>, new()
+    public static async IAsyncEnumerable<T> ReadEntitiesAsync<T>(DbDataReader reader, [EnumeratorCancellation] CancellationToken cancellationToken = default) where T : IMapped<T>, new()
     {
 #if NET8_0_OR_GREATER
         var columnNames = T.ColumnNames;
@@ -52,6 +50,7 @@ public static class EntityReader
         var mapper = new T();
         var columnNames = mapper.GetColumnNames();
         var ordinals = new int[columnNames.Length];
+        
         for (int i = 0; i < columnNames.Length; i++)
         {
             ordinals[i] = reader.GetOrdinal(columnNames[i]);
@@ -69,14 +68,13 @@ public static class EntityReader
 #endif
 
 #if !NET8_0_OR_GREATER && !ASYNC_ENUMERABLE_SUPPORT
-    public static async Task<List<T>> ReadEntitiesAsync<T>(
-        DbDataReader reader,
-        CancellationToken cancellationToken = default)
+    public static async Task<List<T>> ReadEntitiesAsync<T>(DbDataReader reader, CancellationToken cancellationToken = default)
         where T : IMapped<T>, new()
     {
         var mapper = new T();
         var columnNames = mapper.GetColumnNames();
         var ordinals = new int[columnNames.Length];
+
         for (int i = 0; i < columnNames.Length; i++)
         {
             ordinals[i] = reader.GetOrdinal(columnNames[i]);
@@ -84,9 +82,7 @@ public static class EntityReader
 
         var results = new List<T>();
         while (await reader.ReadAsync(cancellationToken).ConfigureAwait(false))
-        {
             results.Add(mapper.ReadEntity(reader, ordinals));
-        }
         return results;
     }
 #endif
