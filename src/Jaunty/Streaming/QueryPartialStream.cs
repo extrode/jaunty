be2282b8d02ad6a326eef1ea079cbc0af724ca05
Syed Beparey <syed@beparey.com>
2026-01-24 20:36:54 +0800

    Removed the new static methods and went back to old school static methods.

--- src/Jaunty/Streaming/QueryPartialStream.cs
+++ src/Jaunty/Streaming/QueryPartialStream.cs
@@ -7,26 +7,23 @@ namespace Jaunty;
 
 public static partial class Jaunty
 {
-    extension(IDbConnection connection)
+    public static IEnumerable<T> QueryPartialStream<T>(this IDbConnection connection, string sql) where T : new()
     {
-        public IEnumerable<T> QueryPartialStream<T>(string sql) where T : new()
-        {
-            return QueryStreamCore<T>(connection, sql, null, default, MappingMode.Projection);
-        }
+        return QueryStreamCore<T>(connection, sql, null, default, MappingMode.Projection);
+    }
 
-        public IEnumerable<T> QueryPartialStream<T>(string sql, object parameters) where T : new()
-        {
-            return QueryStreamCore<T>(connection, sql, parameters, default, MappingMode.Projection);
-        }
+    public static IEnumerable<T> QueryPartialStream<T>(this IDbConnection connection, string sql, object parameters) where T : new()
+    {
+        return QueryStreamCore<T>(connection, sql, parameters, default, MappingMode.Projection);
+    }
 
-        public IEnumerable<T> QueryPartialStream<T>(string sql, CommandOptions<T> options) where T : new()
-        {
-            return QueryStreamCore<T>(connection, sql, null, options, MappingMode.Projection);
-        }
+    public static IEnumerable<T> QueryPartialStream<T>(this IDbConnection connection, string sql, CommandOptions<T> options) where T : new()
+    {
+        return QueryStreamCore<T>(connection, sql, null, options, MappingMode.Projection);
+    }
 
-        public IEnumerable<T> QueryPartialStream<T>(string sql, object parameters, CommandOptions<T> options) where T : new()
-        {
-            return QueryStreamCore<T>(connection, sql, parameters, options, MappingMode.Projection);
-        }
+    public static IEnumerable<T> QueryPartialStream<T>(this IDbConnection connection, string sql, object parameters, CommandOptions<T> options) where T : new()
+    {
+        return QueryStreamCore<T>(connection, sql, parameters, options, MappingMode.Projection);
     }
 }
--- src/Jaunty/Streaming/QueryUnbuffered.cs
+++ src/Jaunty/Streaming/QueryUnbuffered.cs
@@ -7,26 +7,23 @@ namespace Jaunty;
 
 public static partial class Jaunty
 {
-    extension(IDbConnection connection)
+    public static IEnumerable<T> QueryUnbuffered<T>(this IDbConnection connection, string sql) where T : new()
     {
-        public IEnumerable<T> QueryUnbuffered<T>(string sql) where T : new()
-        {
-            return QueryStreamCore<T>(connection, sql, null, default, MappingMode.Strict);
-        }
+        return QueryStreamCore<T>(connection, sql, null, default, MappingMode.Strict);
+    }
 
-        public IEnumerable<T> QueryUnbuffered<T>(string sql, object parameters) where T : new()
-        {
-            return QueryStreamCore<T>(connection, sql, parameters, default, MappingMode.Strict);
-        }
+    public static IEnumerable<T> QueryUnbuffered<T>(this IDbConnection connection, string sql, object parameters) where T : new()
+    {
+        return QueryStreamCore<T>(connection, sql, parameters, default, MappingMode.Strict);
+    }
 
-        public IEnumerable<T> QueryUnbuffered<T>(string sql, CommandOptions<T> options) where T : new()
-        {
-            return QueryStreamCore<T>(connection, sql, null, options, MappingMode.Strict);
-        }
+    public static IEnumerable<T> QueryUnbuffered<T>(this IDbConnection connection, string sql, CommandOptions<T> options) where T : new()
+    {
+        return QueryStreamCore<T>(connection, sql, null, options, MappingMode.Strict);
+    }
 
-        public IEnumerable<T> QueryUnbuffered<T>(string sql, object parameters, CommandOptions<T> options) where T : new()
-        {
-            return QueryStreamCore<T>(connection, sql, parameters, options, MappingMode.Strict);
-        }
+    public static IEnumerable<T> QueryUnbuffered<T>(this IDbConnection connection, string sql, object parameters, CommandOptions<T> options) where T : new()
+    {
+        return QueryStreamCore<T>(connection, sql, parameters, options, MappingMode.Strict);
     }
 }
