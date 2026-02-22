8154ef1d3e923501292eebf6c9699c73ddf72085
Syed Beparey <syed@beparey.com>
2026-02-22 19:02:37 +0800

    test: convert the stored-procedure wrappers to dialect theories
     1. Converted stored-procedure wrapper tests in bulk to dialect DataAttribute theories:
    
     - tests/Jaunty.Tests/Integration/SqlServer/StoredProcedure/SqlServerStoredProcedureTests.cs
     - tests/Jaunty.Tests/Integration/SqlServer/StoredProcedure/SqlServerStoredProcedureAsyncTests.cs
     - tests/Jaunty.Tests/Integration/Postgres/StoredProcedure/PostgresStoredProcedureTests.cs
     - tests/Jaunty.Tests/Integration/Postgres/StoredProcedure/PostgresStoredProcedureAsyncTests.cs
     - tests/Jaunty.Tests/Integration/MySql/StoredProcedure/MySqlStoredProcedureTests.cs
     - tests/Jaunty.Tests/Integration/MySql/StoredProcedure/MySqlStoredProcedureAsyncTests.cs
    
     Applied globally:
    
     - SkipIfNoSqlServerFact -> [Theory] [SqlServer]
     - SkipIfNoPostgresFact -> [Theory] [Postgres]
     - SkipIfNoMySqlFact -> [Theory] [MariaDB]
     - Method signatures updated from () to (DialectInfo _) in those wrappers.
    
     2. Removed obsolete helper files in one batch:
    
     - tests/Jaunty.Tests/Helpers/SkipIfNoSqlServerAttribute.cs
     - tests/Jaunty.Tests/Helpers/SkipIfNoPostgresAttribute.cs
     - tests/Jaunty.Tests/Helpers/SkipIfNoMySqlAttribute.cs
     - tests/Jaunty.Tests/Helpers/SkipSQLiteAsyncAttribute.cs
     - tests/Jaunty.Tests/Helpers/SQLiteTestBase.cs
    
     3. Updated tests/Jaunty.Tests/the contributor guide to remove references to deleted skip/base helpers and align
     language with dialect DataAttribute usage.
     4.
    
     Current status is now focused to these files:
    
     - tests/Jaunty.Tests/the contributor guide (modified)
     - tests/Jaunty.Tests/GlobalUsings.cs (modified, kept)
     - six stored-procedure wrapper files (modified)
     - five helper files above (deleted)
    
     Remaining optional cleanup:
    
     - Database.cs is still needed by tests/Jaunty.Tests/Integration/Sqlite/* source tests, so I did not
     delete it.

--- tests/Jaunty.Tests/Integration/SqlServer/StoredProcedure/SqlServerStoredProcedureTests.cs
+++ tests/Jaunty.Tests/Integration/SqlServer/StoredProcedure/SqlServerStoredProcedureTests.cs
@@ -1,6 +1,7 @@
 using System.Data;
 using Microsoft.Data.SqlClient;
 using Jaunty.Tests.Helpers;
+using Jaunty.Tests.Helpers.Dialects;
 
 namespace Jaunty.Tests.Integration.SqlServer.StoredProcedure;
 
@@ -17,63 +18,79 @@ public class SqlServerStoredProcedureTests : Integration.StoredProcedure.StoredP
     protected override IDbConnection CreateConnection() =>
         new SqlConnection(TestConfiguration.SqlServerConnectionString);
 
-    [SkipIfNoSqlServerFact]
-    public void ExecuteStoredProcedure_WithResults_ReturnsEntities() =>
+    [Theory]
+    [SqlServer]
+    public void ExecuteStoredProcedure_WithResults_ReturnsEntities(DialectInfo _) =>
         ExecuteStoredProcedure_WithResults_ReturnsEntities_Core();
 
-    [SkipIfNoSqlServerFact]
-    public void ExecuteStoredProcedure_WithParameters_ReturnsFilteredResults() =>
+    [Theory]
+    [SqlServer]
+    public void ExecuteStoredProcedure_WithParameters_ReturnsFilteredResults(DialectInfo _) =>
         ExecuteStoredProcedure_WithParameters_ReturnsFilteredResults_Core();
 
-    [SkipIfNoSqlServerFact]
-    public void ExecuteStoredProcedure_WithParametersAndOptions_Works() =>
+    [Theory]
+    [SqlServer]
+    public void ExecuteStoredProcedure_WithParametersAndOptions_Works(DialectInfo _) =>
         ExecuteStoredProcedure_WithParametersAndOptions_Works_Core();
 
-    [SkipIfNoSqlServerFact]
-    public void ExecuteStoredProcedureFirst_WithResults_ReturnsFirst() =>
+    [Theory]
+    [SqlServer]
+    public void ExecuteStoredProcedureFirst_WithResults_ReturnsFirst(DialectInfo _) =>
         ExecuteStoredProcedureFirst_WithResults_ReturnsFirst_Core();
 
-    [SkipIfNoSqlServerFact]
-    public void ExecuteStoredProcedureFirst_WithParametersAndOptions_Works() =>
+    [Theory]
+    [SqlServer]
+    public void ExecuteStoredProcedureFirst_WithParametersAndOptions_Works(DialectInfo _) =>
         ExecuteStoredProcedureFirst_WithParametersAndOptions_Works_Core();
 
-    [SkipIfNoSqlServerFact]
-    public void ExecuteStoredProcedureFirst_NoResults_Throws() =>
+    [Theory]
+    [SqlServer]
+    public void ExecuteStoredProcedureFirst_NoResults_Throws(DialectInfo _) =>
         ExecuteStoredProcedureFirst_NoResults_Throws_Core();
 
-    [SkipIfNoSqlServerFact]
-    public void ExecuteStoredProcedureFirstOrDefault_WithResults_ReturnsFirst() =>
+    [Theory]
+    [SqlServer]
+    public void ExecuteStoredProcedureFirstOrDefault_WithResults_ReturnsFirst(DialectInfo _) =>
         ExecuteStoredProcedureFirstOrDefault_WithResults_ReturnsFirst_Core();
 
-    [SkipIfNoSqlServerFact]
-    public void ExecuteStoredProcedureFirstOrDefault_NoResults_ReturnsNull() =>
+    [Theory]
+    [SqlServer]
+    public void ExecuteStoredProcedureFirstOrDefault_NoResults_ReturnsNull(DialectInfo _) =>
         ExecuteStoredProcedureFirstOrDefault_NoResults_ReturnsNull_Core();
 
-    [SkipIfNoSqlServerFact]
-    public void ExecuteStoredProcedureFirstOrDefault_WithParametersAndOptions_Works() =>
+    [Theory]
+    [SqlServer]
+    public void ExecuteStoredProcedureFirstOrDefault_WithParametersAndOptions_Works(DialectInfo _) =>
         ExecuteStoredProcedureFirstOrDefault_WithParametersAndOptions_Works_Core();
 
-    [SkipIfNoSqlServerFact]
-    public void ExecuteStoredProcedureScalar_ReturnsScalarValue() =>
+    [Theory]
+    [SqlServer]
+    public void ExecuteStoredProcedureScalar_ReturnsScalarValue(DialectInfo _) =>
         ExecuteStoredProcedureScalar_ReturnsScalarValue_Core();
 
-    [SkipIfNoSqlServerFact]
-    public void ExecuteStoredProcedureScalar_WithParameters_ReturnsValue() =>
+    [Theory]
+    [SqlServer]
+    public void ExecuteStoredProcedureScalar_WithParameters_ReturnsValue(DialectInfo _) =>
         ExecuteStoredProcedureScalar_WithParameters_ReturnsValue_Core();
 
-    [SkipIfNoSqlServerFact]
-    public void ExecuteStoredProcedureScalar_WithParametersAndOptions_Works() =>
+    [Theory]
+    [SqlServer]
+    public void ExecuteStoredProcedureScalar_WithParametersAndOptions_Works(DialectInfo _) =>
         ExecuteStoredProcedureScalar_WithParametersAndOptions_Works_Core();
 
-    [SkipIfNoSqlServerFact]
-    public void ExecuteStoredProcedureNonQuery_ExecutesSuccessfully() =>
+    [Theory]
+    [SqlServer]
+    public void ExecuteStoredProcedureNonQuery_ExecutesSuccessfully(DialectInfo _) =>
         ExecuteStoredProcedureNonQuery_ExecutesSuccessfully_Core();
 
-    [SkipIfNoSqlServerFact]
-    public void ExecuteStoredProcedureNonQuery_WithOutputParameter_ReturnsOutputValue() =>
+    [Theory]
+    [SqlServer]
+    public void ExecuteStoredProcedureNonQuery_WithOutputParameter_ReturnsOutputValue(DialectInfo _) =>
         ExecuteStoredProcedureNonQuery_WithOutputParameter_ReturnsOutputValue_Core();
 
-    [SkipIfNoSqlServerFact]
-    public void SpParameters_HasValue_ReturnsTrueForOutputWithValue() =>
+    [Theory]
+    [SqlServer]
+    public void SpParameters_HasValue_ReturnsTrueForOutputWithValue(DialectInfo _) =>
         SpParameters_HasValue_ReturnsTrueForOutputWithValue_Core();
 }
+
