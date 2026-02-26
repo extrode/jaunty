049e0debb8686f0a53f72b969ad76716ea21e278
Syed Beparey <syed@beparey.com>
2026-02-26 09:34:37 +0800

    Problem fixed: MicrosoftSqlite tests no longer pollute the test output on .NET Framework.
     `MicrosoftSqliteAttribute.cs` - Added conditional compilation:
     - net8.0+: Normal behavior - tests run and may skip if SQLite is unavailable
     - net472: Returns empty test data - tests are completely excluded from discovery

--- tests/Jaunty.Tests/Helpers/Dialects/MicrosoftSqliteAttribute.cs
+++ tests/Jaunty.Tests/Helpers/Dialects/MicrosoftSqliteAttribute.cs
@@ -1,3 +1,4 @@
+#if NET8_0_OR_GREATER
 using Microsoft.Data.Sqlite;
 
 namespace Jaunty.Tests.Helpers.Dialects;
@@ -30,3 +31,28 @@ public sealed class MicrosoftSqliteAttribute : DialectDataAttributeBase
         }
     }
 }
+#else
+namespace Jaunty.Tests.Helpers.Dialects;
+
+/// <summary>
+/// Placeholder for MicrosoftSqliteAttribute on .NET Framework.
+/// Returns no test data to completely exclude tests from discovery.
+/// </summary>
+public sealed class MicrosoftSqliteAttribute : DialectDataAttributeBase
+{
+    public MicrosoftSqliteAttribute()
+        : base(string.Empty)
+    {
+    }
+
+    protected override bool IsAvailable => false;
+
+    protected override DialectInfo Dialect => DialectInfo.MicrosoftSqlite;
+
+    public override System.Collections.Generic.IEnumerable<object[]> GetData(System.Reflection.MethodInfo testMethod)
+    {
+        // Return no data on .NET Framework to exclude tests from discovery entirely
+        return System.Linq.Enumerable.Empty<object[]>();
+    }
+}
+#endif
