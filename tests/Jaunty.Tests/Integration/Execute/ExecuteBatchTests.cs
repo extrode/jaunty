f2f19ba5ccfffa36a35ab09b629fed4c8fa6321b
Syed Beparey <syed@beparey.com>
2026-07-03 10:04:58 +0800

    feat(Jaunty): T009-T016 - Implement Execute, ExecuteAsync, ExecuteBatch with tests

---
+++ tests/Jaunty.Tests/Integration/Execute/ExecuteBatchTests.cs
@@ -0,0 +1,92 @@
+using Jaunty.Core;
+using Jaunty.Tests.Entities;
+using Jaunty.Tests.Helpers.Dialects;
+
+namespace Jaunty.Tests.Integration.Execute;
+
+[Collection("Execute Operations")]
+public class ExecuteBatchTests : IClassFixture<DialectFixture>
+{
+    private readonly DialectFixture _fixture;
+
+    public ExecuteBatchTests(DialectFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    private static int GetRowCount(System.Data.IDbConnection connection)
+    {
+        using var cmd = connection.CreateCommand();
+        cmd.CommandText = "SELECT COUNT(*) FROM bulk_test";
+        return Convert.ToInt32(cmd.ExecuteScalar());
+    }
+
+    [Theory]
+    [SqlServer]
+    [Postgres]
+    [MariaDB]
+    [MicrosoftSqlite]
+    [SystemSqlite]
+    public void ExecuteBatch_MultipleInserts_ReturnsCumulativeRows(DialectInfo dialect)
+    {
+        using var ctx = _fixture.GetWriteContext(dialect);
+        var paramSets = new object[]
+        {
+            new { Name = "Test1", Value = 100 },
+            new { Name = "Test2", Value = 200 },
+            new { Name = "Test3", Value = 300 }
+        };
+
+        var totalRows = ctx.Connection.ExecuteBatch(
+            "INSERT INTO bulk_test (name, value) VALUES (@Name, @Value)",
+            paramSets);
+
+        Assert.Equal(3, totalRows);
+        Assert.Equal(3, GetRowCount(ctx.Connection));
+    }
+
+    [Theory]
+    [SqlServer]
+    [Postgres]
+    [MariaDB]
+    [MicrosoftSqlite]
+    [SystemSqlite]
+    public void ExecuteBatch_WithEmptyList_ReturnsZero(DialectInfo dialect)
+    {
+        using var ctx = _fixture.GetWriteContext(dialect);
+        var paramSets = new object[] { };
+
+        var totalRows = ctx.Connection.ExecuteBatch(
+            "INSERT INTO bulk_test (name, value) VALUES (@Name, @Value)",
+            paramSets);
+
+        Assert.Equal(0, totalRows);
+    }
+
+    [Theory]
+    [SqlServer]
+    [Postgres]
+    [MariaDB]
+    [MicrosoftSqlite]
+    [SystemSqlite]
+    public void ExecuteBatch_WithTransaction_RollbackUndoesAll(DialectInfo dialect)
+    {
+        using var ctx = _fixture.GetWriteContext(dialect);
+        using var tx = ctx.Connection.BeginTransaction();
+        var paramSets = new object[]
+        {
+            new { Name = "Batch1", Value = 100 },
+            new { Name = "Batch2", Value = 200 }
+        };
+
+        var totalRows = ctx.Connection.ExecuteBatch(
+            "INSERT INTO bulk_test (name, value) VALUES (@Name, @Value)",
+            paramSets,
+            CommandOptions.WithTransaction(tx));
+
+        Assert.Equal(2, totalRows);
+        tx.Rollback();
+        
+        Assert.Equal(0, GetRowCount(ctx.Connection));
+    }
+}
